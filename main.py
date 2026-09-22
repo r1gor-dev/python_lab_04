bf52c3ea92ba258d545330abdda9ba6b2b6fac17
r1gor-dev <r1gor@bk.ru>
2026-09-22 04:45:47 +0300

    work with pathlib

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from utils.statistics import calculate_average, find_min, find_max
 from utils.reader import read_numbers
-# from utils.statistics import find_min
-# from utils.statistics import find_max
+from pathlib import Path
+
 def main():
+    cur_dir = Path.cwd()
+    print(cur_dir)
+
+    filepath = Path("data/measurements.txt")
+    print(filepath.exists())
+
+    logsdir = Path("logs")
+    logsdir.mkdir(exist_ok=True)
+
     a =read_numbers("data/measurements.txt")
     print(calculate_average(a))
     print(find_min(a))
